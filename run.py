9bcfba4dccc0b31612b81c537f8c8e474b0518a5
ExcaliburZero <cwellsny@nycap.rr.com>
2015-11-01 16:09:13 -0500

    Altered the implementation of the run script
    Changed the way in which the run script ran the server and speedtest script, as the original method did not work on an Arch Linux Raspberry Pi.

--- run.py
+++ run.py
@@ -3,14 +3,16 @@ from subprocess import Popen, PIPE
 import time
 
 # Start the server
-server_terminal = Popen("python -m SimpleHTTPServer 8080", shell=True, stdin=PIPE, stdout=PIPE)
+#server_terminal = Popen("python -m SimpleHTTPServer 8080", shell=True, stdin=PIPE, stdout=PIPE)
+server_terminal = Popen("python3 -m http.server 8080", shell=True, stdin=PIPE, stdout=PIPE)
 
 # Run the get speed command evey x minutes y times
 x = 5
 y = 120
 i = 0
 while i < y:
-	Popen("python speedtest_cli.py --csv bandwidth_report.csv", shell=True, stdin=PIPE, stdout=PIPE)
+	#Popen("python speedtest_cli.py --csv bandwidth_report.csv", shell=True, stdin=PIPE, stdout=PIPE)
+	Popen("python3 speedtest_cli.py --csv bandwidth_report.csv", shell=True, stdin=PIPE, stdout=PIPE)
 	print("Got time: " + str(x * (i + 1)))
 	time.sleep(x * 60);
 	i = i + 1
